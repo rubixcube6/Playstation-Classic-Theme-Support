1f9c0e0fca26fda85d3d615d5bd947a231922532
Mason Barry <creativemessss@gmail.com>
2018-12-12 10:59:35 -0600

    Revert "."
    This reverts commit 423f163da4b4621fc2f524afaef287c9cac46b4c.

--- lolhack/lolhack.sh
+++ lolhack/lolhack.sh
@@ -4,6 +4,4 @@
 #The theme only works when the USB Drive is 
 #plugged in. Un-plug the power cable and reboot
 #without the USB drive and it will go back to normal
-mount -o bind /media/theme/images /usr/sony/share/data/images
-mount -o bind /media/theme/sounds /usr/sony/share/data/sounds
-mount -o bind /media/theme/font /usr/sony/share/data/font
+mount -o bind /media/theme/ /usr/sony/share/data/
